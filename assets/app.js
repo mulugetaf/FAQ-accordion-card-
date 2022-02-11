e814e14532329aa5e8b813889c6b1346b3d64a47
mulugetaf <mulukas@gmail.com>
2022-02-11 14:31:19 +0200

    Initial commit

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+
+
+// document.onclick = function(){myFunction()};
+
+function myFunction(event){
+    // name = document.getElementsByClassName(this)
+    console.log(event.target.getAttribute('id'));
+
+}
